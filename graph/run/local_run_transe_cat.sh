dcd84946296d418a9c890964e905c40798d8dc12
Fernando Zhapa <ferynando7@gmail.com>
2022-10-29 13:11:51 +0300

    .

---
+++ graph/run/local_run_transe_cat.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for epochs_f in 20 40 60
+do
+    for emb_size in 50 100 200
+    do
+	python run_cge_transe.py -case go -g categorical -epf ${epochs_f} -esize ${emb_size} -eps 1 -lr 1 --margin 1 -dev cuda -train
+    done
+done
