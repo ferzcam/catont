97565da84db5c1960971c9df2568a95eb9213aa7
Fernando Zhapa <ferynando7@gmail.com>
2022-11-03 04:09:19 +0300

    .

--- graph/run/analyze_results_all.py
+++ graph/run/analyze_results_all.py
@@ -6,80 +6,83 @@ def analyze_results(root):
     categorical_file = root + "categorical_results"
 
     df_owl2vec = pd.read_csv(owl2vec_star_file, header=None)
+    df_cat = pd.read_csv(categorical_file, header=None)
+    
+    if "foodon" in root:
+        df_owl2vec.columns = ["num_walks", "walk_length", "alpha", "w2v_epochs", "window_size", "embedding_\
+size", "mlp_epochs", "hits1", "hits5", "hits10", "mrr", "fhits1", "fhits5", "fhits10", "fmrr"]
 
-    df_owl2vec.columns = ["num_walks", "walk_length", "alpha", "w2v_epochs", "window_size", "embedding_\
+        df_cat.columns = ["num_walks", "walk_length", "alpha", "w2v_epochs", "window_size", "embedding_size\
+", "mlp_epochs", "hits1", "hits5", "hits10", "mrr", "fhits1", "fhits5", "fhits10", "fmrr"]
+
+    else:
+        df_owl2vec.columns = ["num_walks", "walk_length", "alpha", "w2v_epochs", "window_size", "embedding_\
 size", "hits1", "hits5", "hits10", "mrr", "fhits1", "fhits5", "fhits10", "fmrr"]
 
-    df_cat = pd.read_csv(categorical_file, header=None)
-
-    df_cat.columns = ["num_walks", "walk_length", "alpha", "w2v_epochs", "window_size", "embedding_size\
+        df_cat.columns = ["num_walks", "walk_length", "alpha", "w2v_epochs", "window_size", "embedding_size\
 ", "hits1", "hits5", "hits10", "mrr", "fhits1", "fhits5", "fhits10", "fmrr"]
 
 
+    
+
     #Get best hits1
-    best_owl2vec = df_owl2vec.loc[df_owl2vec['hits1'].idxmax()]
+    best_owl2vec = df_owl2vec.loc[df_owl2vec['hits1'].idxmax()].to_frame().T
     best_cat = df_cat.loc[df_cat['hits1'].idxmax()]
+    new_df = best_owl2vec.append(best_cat)
 
-    #print all the row at the best indices
-    print("Hits@1")
-    print(best_owl2vec)
-    print(best_cat)
+    new_df.to_csv(root + "best_results", index=False)
 
+                
     #Get best hits5
     best_owl2vec = df_owl2vec.loc[df_owl2vec['hits5'].idxmax()]
     best_cat = df_cat.loc[df_cat['hits5'].idxmax()]
 
-    print("Hits@5")
-    print(best_owl2vec)
-    print(best_cat)
-
+    new_df = new_df.append(best_owl2vec)
+    new_df = new_df.append(best_cat)
+            
     #Get best hits10
     best_owl2vec = df_owl2vec.loc[df_owl2vec['hits10'].idxmax()]
     best_cat = df_cat.loc[df_cat['hits10'].idxmax()]
 
-    print("Hits@10")
-    print(best_owl2vec)
-    print(best_cat)
-
+    new_df = new_df.append(best_owl2vec)
+    new_df = new_df.append(best_cat)
+            
     #Get best mrr
     best_owl2vec = df_owl2vec.loc[df_owl2vec['mrr'].idxmax()]
     best_cat = df_cat.loc[df_cat['mrr'].idxmax()]
 
-    print("MRR")
-    print(best_owl2vec)
-    print(best_cat)
-            
+    new_df = new_df.append(best_owl2vec)
+    new_df = new_df.append(best_cat)
+                
     #Get best fhits1
     best_owl2vec = df_owl2vec.loc[df_owl2vec['fhits1'].idxmax()]
     best_cat = df_cat.loc[df_cat['fhits1'].idxmax()]
 
-    print("Filtered Hits@1")
-    print(best_owl2vec)
-    print(best_cat)
-
+    new_df = new_df.append(best_owl2vec)
+    new_df = new_df.append(best_cat)
+            
     #Get best fhits5
     best_owl2vec = df_owl2vec.loc[df_owl2vec['fhits5'].idxmax()]
     best_cat = df_cat.loc[df_cat['fhits5'].idxmax()]
 
-    print("Filtered Hits@5")
-    print(best_owl2vec)
-    print(best_cat)
-    
+    new_df = new_df.append(best_owl2vec)
+    new_df = new_df.append(best_cat)
+            
     #Get best fhits10
     best_owl2vec = df_owl2vec.loc[df_owl2vec['fhits10'].idxmax()]
     best_cat = df_cat.loc[df_cat['fhits10'].idxmax()]
 
-    print("Filtered Hits@10")
-    print(best_owl2vec)
-    print(best_cat)
-            
+    new_df = new_df.append(best_owl2vec)
+    new_df = new_df.append(best_cat)
+                
     #Get best fmrr
     best_owl2vec = df_owl2vec.loc[df_owl2vec['fmrr'].idxmax()]
     best_cat = df_cat.loc[df_cat['fmrr'].idxmax()]
 
-    print("Filtered MRR")
-    print(best_owl2vec)
-    print(best_cat)
+    new_df = new_df.append(best_owl2vec)
+    new_df = new_df.append(best_cat)
+
+    new_df.round(6).to_csv(root + "best_results", index=False)
             
 if __name__ == "__main__":                                                                              
     case = sys.argv[1]                                                                                  
